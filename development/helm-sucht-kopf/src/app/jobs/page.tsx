661384b9723a87cda59454d09a967562675d1769
Nam <namkhanh240@gmail.com>
2024-12-06 20:16:58 +0100

    Refactor filters UI and improve job card links
    Replaced checkboxes with a dropdown for filter selection and added a reset button to clear filters. Converted image and text links within job cards to use anchor tags for better semantic structure and consistency. Overall, streamlined the UI for a more user-friendly experience on the jobs page.

--- development/helm-sucht-kopf/src/app/jobs/page.tsx
+++ development/helm-sucht-kopf/src/app/jobs/page.tsx
@@ -28,13 +28,7 @@ export default function jobsPage() {
         };
         getJobs().then(r => console.log(r));
     }, []);
-
-    const handleFilterChange = (filter: string) => {
-        setSelectedFilters(prev =>
-            prev.includes(filter) ? prev.filter(f => f !== filter) : [...prev, filter]
-        );
-    };
-
+    
     const handleReset = () => {
         setSelectedFilters([]);
     };
@@ -44,8 +38,8 @@ export default function jobsPage() {
     );
 
     return (
-        <div className="flex flex-col relative md:pt-[2vh] md:pr-[5vh] md:pb-[5vh] md:pl-[5vh]
-        font-[family-name:var(--font-berlin-type-regular)]">
+        <div className="flex flex-col relative md:pt-[2vh] md:pr-[5vh] md:pb-[5vh] md:pl-[5vh]"
+             style={{fontFamily: 'var(--font-berlin-type-regular)'}}>
 
             <div style={{width: '100%', padding: '1.5rem'}}>
                 <h1 style={{
@@ -53,10 +47,11 @@ export default function jobsPage() {
                     fontSize: '2rem',
                     fontWeight: 'bold',
                     textAlign: 'left',
-                    fontFamily: 'var(--font-berlin-type-bold)'}}
+                    fontFamily: 'var(--font-berlin-type-bold)'
+                }}
                 >
                     Dein Weg bei der Berliner Feuerwehr</h1>
-                <p style={{textAlign: 'left', marginTop: '1.5rem'}}>
+                <p style={{textAlign: 'left', marginTop: '0.5rem'}}>
                     Finde deinen idealen Karriereweg bei der Feuerwehr Berlin. Der Karriere Navigator zeigt dir
                     alle Optionen, Spezialisierungen und Aufstiegschancen – für eine erfolgreiche und spannende
                     Laufbahn.
@@ -84,28 +79,60 @@ export default function jobsPage() {
                     paddingLeft: '1rem',
                     paddingRight: '1rem',
                     marginTop: '2rem',
-                    alignSelf: 'flex-start'}}
-                     className="md:w-1/4 md:border md:rounded-lg">
-                    <div className="hidden md:block">
-                        {filters.map(({key, label}) => (
-                            <label key={key} style={{padding: '2px', display: 'block'}}>
-                                <input
-                                    type="checkbox"
-                                    checked={selectedFilters.includes(key)}
-                                    onChange={() => handleFilterChange(key)}
-                                    style={{padding: '2px', marginRight: '2px'}}
-                                />
-                                {label}
-                            </label>
-                        ))}
+                    alignSelf: 'flex-start'
+                }}
+                     className="hidden md:block md:w-1/4 md:border md:rounded-lg">
+
+                    <div style={{
+                        fontSize: '1.2rem',
+                        fontWeight: 'bold',
+                        fontFamily: 'var(--font-berlin-type-bold)',
+                        marginBottom: '1rem'
+                    }}>
+                        Ergebnisse: {filteredJobs.length}
                     </div>
+                    <div className="block p-2">
+                        <select
+                            value={selectedFilters[0] || ""}
+                            onChange={(e) => setSelectedFilters([e.target.value])}
+                            className="p-2 mr-2 block w-full rounded-xl border-black border"
+                        >
+                            <option value="" disabled>Schulabschluss</option>
+                            {filters.map(({key, label}) => (
+                                <option key={key} value={key} className="p-2">
+                                    {label}
+                                </option>
+                            ))}
+                        </select>
+                    </div>
+                    <button
+                        style={{
+                            backgroundColor: 'var(--red-primary)',
+                            marginTop: '1rem',
+                            padding: '0.5rem',
+                            borderRadius: '1rem',
+                            color: 'white',
+                            width: '50%',
+                            display: 'block',
+                            marginLeft: 'auto',
+                            marginRight: 'auto',
+                        }}
+                        onClick={handleReset}
+                    >
+                        Reset
+                    </button>
                 </div>
                 <div style={{display: 'flex', flexDirection: 'column', flexGrow: '1'}}>
                     <div className="p-2 flex justify-between md:hidden">
                         <div
-                            style={{fontSize: '1.2rem', padding: '0.5rem', fontWeight: 'bold', fontFamily: 'var(--font-berlin-type-bold)'}}
+                            style={{
+                                fontSize: '1.2rem',
+                                padding: '0.5rem',
+                                fontWeight: 'bold',
+                                fontFamily: 'var(--font-berlin-type-bold)'
+                            }}
                         >
-                            {filteredJobs.length} Ergebnisse
+                            Ergebnisse: {filteredJobs.length}
                         </div>
                         <button
                             style={{
@@ -120,29 +147,36 @@ export default function jobsPage() {
                         >
                             Filtern
                         </button>
+                        <button
+                            style={{
+                                borderRadius: '1.5rem',
+                                border: '1px solid black',
+                                width: '30%',
+                                padding: '0.5rem',
+                                backgroundColor: '#E5E7EB',
+                                color: 'var(--red-primary)',
+                                display: isFiltersVisible ? 'block' : 'none'
+                            }}
+                            onClick={() => setIsFiltersVisible(false)}
+                        >
+                            Close
+                        </button>
                     </div>
                     {isFiltersVisible && (
-                        <div className="bg-white p-4 border rounded mb-4">
-                            <div className="flex justify-end mb-2">
-                                <button
-                                    className="p-2 bg-blue-500 text-white rounded"
-                                    onClick={() => setIsFiltersVisible(false)}
+                        <div className="bg-white p-4 border rounded mb-4 md:hidden">
+                            <div className="block p-2" style={{display: 'flex', justifyContent: 'center'}}>
+                                <select
+                                    value={selectedFilters[0] || ""}
+                                    onChange={(e) => setSelectedFilters([e.target.value])}
+                                    className="p-2 mr-2 block w-4/5 border-black border rounded-xl"
                                 >
-                                    Close
-                                </button>
-                            </div>
-                            <div>
-                                {filters.map(({key, label}) => (
-                                    <label key={key} className="block p-2">
-                                        <input
-                                            type="checkbox"
-                                            checked={selectedFilters.includes(key)}
-                                            onChange={() => handleFilterChange(key)}
-                                            className="p-2 mr-2"
-                                        />
-                                        {label}
-                                    </label>
-                                ))}
+                                    <option value="" disabled>Schulabschluss</option>
+                                    {filters.map(({key, label}) => (
+                                        <option key={key} value={key} className="p-2">
+                                            {label}
+                                        </option>
+                                    ))}
+                                </select>
                             </div>
                             <button
                                 style={{
@@ -163,27 +197,30 @@ export default function jobsPage() {
                         </div>
                     )}
                     <div className="job-list grid-cols-1 md:grid-cols-2"
-                        style={{
-                            display: 'grid',
-                            gap: '2rem',
-                            paddingLeft: '2.5rem',
-                            paddingRight: '2.5rem',
-                            flexGrow: '1'}}>
+                         style={{
+                             display: 'grid',
+                             gap: '2rem',
+                             paddingLeft: '2.5rem',
+                             paddingRight: '2.5rem',
+                             flexGrow: '1'
+                         }}>
                         {filteredJobs.map(({id, imageUrl, name, slug, description}) => (
                             <div
                                 key={id}
                                 className="job-card flex flex-col md:flex-row md:items-center mt-8
                                 space-x-4 border rounded-lg relative hover:border-transparent"
                             >
-                                <img
-                                    src={imageUrl}
-                                    alt={name}
-                                    className="job-image w-full md:w-80 rounded-t-lg md:rounded-l-lg md:rounded-tr-none rounded-bl-none"
-                                    style={{
-                                        cursor: 'pointer'
-                                    }}
-                                    onClick={() => router?.push(`/jobs/${slug}`)}
-                                />
+                                <a href={`/jobs/${slug}`}>
+                                    <img
+                                        src={imageUrl}
+                                        alt={name}
+                                        className="job-image w-full md:w-80 rounded-t-lg md:rounded-l-lg md:rounded-tr-none rounded-bl-none"
+                                        style={{
+                                            cursor: 'pointer'
+                                        }}
+                                    />
+                                </a>
+
                                 <div className="flex flex-col justify-between flex-grow relative">
                                     <h3
                                         style={{
@@ -233,35 +270,37 @@ export default function jobsPage() {
                                     }}
                                 >
                                     <p>{description}</p>
-                                    <span
-                                        style={{
-                                            marginTop: '0.5rem',
-                                            paddingBottom: '0.5rem',
-                                            color: 'var(--red-primary)',
-                                            textDecorationLine: 'underline',
-                                            cursor: 'pointer'
-                                        }}
-                                        onClick={() => router?.push(`/jobs/${slug}`)}
-                                    >
-                                        Mehr Infos
-                                    </span>
+                                    <a href={`/jobs/${slug}`}>
+                                        <span
+                                            style={{
+                                                marginTop: '0.5rem',
+                                                paddingBottom: '0.5rem',
+                                                color: 'var(--red-primary)',
+                                                textDecorationLine: 'underline',
+                                                cursor: 'pointer'
+                                            }}
+                                        >
+                                            Mehr Infos
+                                        </span>
+                                    </a>
                                 </div>
                                 <div id={`desc-${id}`} className="hidden md:hidden transition-all rounded-lg mt-2"
                                      style={{width: '85%'}}>
                                     <p style={{fontSize: '85%'}}>{description}</p>
-                                    <span
-                                        style={{
-                                            marginTop: '0.5rem',
-                                            fontSize: '85%',
-                                            paddingBottom: '0.5rem',
-                                            color: 'var(--red-primary)',
-                                            textDecorationLine: 'underline',
-                                            cursor: 'pointer'
-                                        }}
-                                        onClick={() => router?.push(`/jobs/${slug}`)}
-                                    >
-                                        Mehr Infos
-                                    </span>
+                                    <a href={`/jobs/${slug}`}>
+                                        <span
+                                            style={{
+                                                marginTop: '0.5rem',
+                                                fontSize: '85%',
+                                                paddingBottom: '0.5rem',
+                                                color: 'var(--red-primary)',
+                                                textDecorationLine: 'underline',
+                                                cursor: 'pointer'
+                                            }}
+                                        >
+                                            Mehr Infos
+                                        </span>
+                                    </a>
                                 </div>
                             </div>
                         ))}
