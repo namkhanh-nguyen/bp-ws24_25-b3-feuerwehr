f011f7c02517b56a0fa51439821b97e7b2fd5fd1
Nam <namkhanh240@gmail.com>
2024-12-11 20:58:07 +0100

    beautified JSON data and removed footer until official consultation with the Feuerwehr

--- development/helm-sucht-kopf/src/app/layout.tsx
+++ development/helm-sucht-kopf/src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import React from "react";
-import Image from "next/image";
 
 const berlinTypeWebRegular = localFont({
     src: "./fonts/BerlinTypeWeb-Regular.woff",
@@ -60,54 +59,6 @@ export default function RootLayout({
       </header>
 
       {children}
-
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-          <a
-              className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-              href="https://nextjs.org/learn?utm_source=create-next-index&utm_medium=appdir-template-tw&utm_campaign=create-next-index"
-              target="_blank"
-              rel="noopener noreferrer"
-          >
-              <Image
-                  aria-hidden
-                  src="/file.svg"
-                  alt="File icon"
-                  width={16}
-                  height={16}
-              />
-              Learn
-          </a>
-          <a
-              className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-index&utm_medium=appdir-template-tw&utm_campaign=create-next-index"
-              target="_blank"
-              rel="noopener noreferrer"
-          >
-              <Image
-                  aria-hidden
-                  src="/window.svg"
-                  alt="Window icon"
-                  width={16}
-                  height={16}
-              />
-              Examples
-          </a>
-          <a
-              className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-              href="https://nextjs.org?utm_source=create-next-index&utm_medium=appdir-template-tw&utm_campaign=create-next-index"
-              target="_blank"
-              rel="noopener noreferrer"
-          >
-              <Image
-                  aria-hidden
-                  src="/globe.svg"
-                  alt="Globe icon"
-                  width={16}
-                  height={16}
-              />
-              Go to nextjs.org →
-          </a>
-      </footer>
 
       </body>
       </html>
