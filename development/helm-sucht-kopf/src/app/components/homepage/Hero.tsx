9c6f3da8b323680b457b08736de95af40590c474
Nam <namkhanh240@gmail.com>
2024-12-17 01:16:37 +0100

    changed background image implementation to load the image directly instead of using CSS styling, which is slower and makes the slow load super visible

--- development/helm-sucht-kopf/src/app/components/homepage/Hero.tsx
+++ development/helm-sucht-kopf/src/app/components/homepage/Hero.tsx
@@ -2,19 +2,24 @@ import Link from "next/link";
 
 export default function Hero() {
   return (
-    <div>
-      <section className="homepage-hero">
-        <div className="hero-content">
-          <h1 className="hero-title">Lust auf den besten Job der Welt?</h1>
-          <p className="hero-description">
-            Bist du bereit für Action, Teamgeist und einen Job, der wirklich
-            etwas bewegt? Dann bist du bei der Berliner Feuerwehr genau richtig!
-          </p>
-          <Link href="/jobs" className="hero-button">
-            Zum Navigator
-          </Link>
-        </div>
-      </section>
-    </div>
+      <div>
+        <section className="homepage-hero">
+          <img
+              src="/assets/home/hero-image.jpg"
+              alt="Hero Background"
+              className="hero-background-image"
+          />
+          <div className="hero-content">
+            <h1 className="hero-title">Lust auf den besten Job der Welt?</h1>
+            <p className="hero-description">
+              Bist du bereit für Action, Teamgeist und einen Job, der wirklich
+              etwas bewegt? Dann bist du bei der Berliner Feuerwehr genau richtig!
+            </p>
+            <Link href="/jobs" className="hero-button">
+              Zum Navigator
+            </Link>
+          </div>
+        </section>
+      </div>
   );
 }
