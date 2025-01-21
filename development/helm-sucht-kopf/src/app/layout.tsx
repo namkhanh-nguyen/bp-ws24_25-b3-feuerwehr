c39ef67e8ca3eec8d483dda0a41178c3b9a0af40
Christoph <s0585013@htw-berlin.de>
2025-01-21 22:41:06 +0100

    Add viewport meta tag for responsive design; adjust max-height for homepage hero and hero background image

--- development/helm-sucht-kopf/src/app/layout.tsx
+++ development/helm-sucht-kopf/src/app/layout.tsx
@@ -35,6 +35,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <title>Berliner Feuerwehr: Karriere</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link
           rel="icon"
           type="image/png"
