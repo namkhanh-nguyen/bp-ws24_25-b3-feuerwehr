ebadf62a207690d1f26100d68a4653d3994db68e
Christoph <s0585013@htw-berlin.de>
2025-02-07 00:45:28 +0100

    Footer und Imprssumsänderungen mit Hinweis auf Showtime

--- development/helm-sucht-kopf/src/app/impressum/page.tsx
+++ development/helm-sucht-kopf/src/app/impressum/page.tsx
@@ -1,36 +1,37 @@
 export default function Impressum() {
-    return (
-        <div style={{ fontFamily: "Arial, sans-serif", maxWidth: "900px", margin: "auto", padding: "20px" }}>
+  return (
+    <div
+      style={{
+        maxWidth: "75rem",
+        margin: "auto",
+        paddingTop: "40px",
+        paddingBottom: "20px",
+      }}
+    >
+      {/* Impressum Inhalt */}
+      <main style={{ maxWidth: "75ch", padding: "20px", textAlign: "left" }}>
+        <h1>Impressum</h1>
+        <p>
+          <strong>Verantwortlich für den Inhalt:</strong>
+        </p>
+        <p>
+          Projektteam „Helm sucht Kopf“ – Studierendenprojekt der HTW Berlin
+          (IMI Showtime WS 2024/25)
+        </p>
+        <p>
+          Hochschule für Technik und Wirtschaft (HTW) Berlin
+          <br />
+          Treskowallee 8<br /> 10318 Berlin <br />
+          <a href="http://www.htw-berlin.de">www.htw-berlin.de</a>
+        </p>
 
-            {/* Impressum Inhalt */}
-            <main style={{ padding: "20px", textAlign: "left" }}>
-
-                <h2>Impressum</h2>
-                <p><strong>Verantwortlich für den Inhalt:</strong></p>
-                <p>Projektteam „Helm sucht Kopf“ – Studierendenprojekt der HTW Berlin (IMI Showtime WS 2024/25)</p>
-
-                <h2>Mitwirkende:</h2>
-                <p>
-                    Ein studentisches Projektteam der HTW Berlin im Rahmen von IMI Showtime Wintersemester 2024/25.
-                </p>
-                <h3>Projektteam:</h3>
-                <ul>
-                    <li>Duha</li>
-                    <li>Maria</li>
-                    <li>Sara</li>
-                    <li>Nhi</li>
-                    <li>Christoph</li>
-                    <li>Nam</li>
-                    <li>Sandip</li>
-                </ul>
-
-                <h2>Haftungsausschluss:</h2>
-                <p>
-                    Trotz sorgfältiger inhaltlicher Kontrolle übernehmen wir keine Haftung für die Inhalte externer Links.
-                    Für den Inhalt verlinkter Seiten sind ausschließlich deren Betreiber verantwortlich.
-                </p>
-            </main>
-
-        </div>
-    );
+        <h2>Haftungsausschluss:</h2>
+        <p>
+          Trotz sorgfältiger inhaltlicher Kontrolle übernehmen wir keine Haftung
+          für die Inhalte externer Links. Für den Inhalt verlinkter Seiten sind
+          ausschließlich deren Betreiber verantwortlich.
+        </p>
+      </main>
+    </div>
+  );
 }
