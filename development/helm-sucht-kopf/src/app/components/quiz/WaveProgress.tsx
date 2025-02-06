5c88dd2202f0782265801258aedf104f4f59a66b
Christoph <s0585013@htw-berlin.de>
2025-02-06 22:36:26 +0100

    Quiz: adjusted padding and margins mostly

--- development/helm-sucht-kopf/src/app/components/quiz/WaveProgress.tsx
+++ development/helm-sucht-kopf/src/app/components/quiz/WaveProgress.tsx
@@ -1,88 +1,84 @@
 "use client";
 
 import { useEffect, useRef } from "react";
-import FiremenIcon from '@/app/components/quiz/FiremenIcon';
-import HouseOnFireIcon from '@/app/components/quiz/HouseOnFire';
+import FiremenIcon from "@/app/components/quiz/FiremenIcon";
+import HouseOnFireIcon from "@/app/components/quiz/HouseOnFire";
 
 interface WaveProgressProps {
-    progress: number; // 0 to 100
+  progress: number; // 0 to 100
 }
 
 export default function WaveProgress({ progress }: WaveProgressProps) {
-    const waveRefs = [
-        useRef<SVGPathElement>(null),
-        useRef<SVGPathElement>(null),
-        useRef<SVGPathElement>(null),
-        useRef<SVGPathElement>(null),
-        useRef<SVGPathElement>(null),
-    ];
+  const waveRefs = [
+    useRef<SVGPathElement>(null),
+    useRef<SVGPathElement>(null),
+    useRef<SVGPathElement>(null),
+    useRef<SVGPathElement>(null),
+    useRef<SVGPathElement>(null),
+  ];
 
-    useEffect(() => {
-        waveRefs.forEach((waveRef, index) => {
-            const wave = waveRef.current;
-            if (!wave) return;
+  useEffect(() => {
+    waveRefs.forEach((waveRef, index) => {
+      const wave = waveRef.current;
+      if (!wave) return;
 
-            const length = wave.getTotalLength();
-            wave.style.strokeDasharray = `${length}`;
-            wave.style.strokeDashoffset = `${length * (1 - progress / 100)}`;
-        });
-    }, [progress]);
+      const length = wave.getTotalLength();
+      wave.style.strokeDasharray = `${length}`;
+      wave.style.strokeDashoffset = `${length * (1 - progress / 100)}`;
+    });
+  }, [progress]);
 
-    return (
-        <div className="w-full h-14 flex items-center justify-center bg-transparent relative">
-            {/* SVG container */}
-            <svg
-                viewBox="0 0 600 100"
-                className="w-full"
-                preserveAspectRatio="none"
-            >
-                {/* Firemen Icon at the start */}
-                <foreignObject x="0" y="15" width="70" height="70">
-                    <FiremenIcon />
-                </foreignObject>
+  return (
+    <div className="w-full h-14 flex items-center justify-center bg-transparent relative">
+      {/* SVG container */}
+      <svg viewBox="0 0 600 100" className="w-full" preserveAspectRatio="none">
+        {/* Firemen Icon at the start */}
+        <foreignObject x="0" y="15" width="70" height="70">
+          <FiremenIcon />
+        </foreignObject>
 
-                {/* Waves */}
-                <path
-                    ref={waveRefs[0]}
-                    d="M 70 27 C 150 -3, 150 57, 300 27 C 450 -3, 450 57, 530 27"
-                    stroke="#cfebff"
-                    strokeWidth="3"
-                    fill="none"
-                />
-                <path
-                    ref={waveRefs[1]}
-                    d="M 70 27 C 150 -13, 150 67, 300 27 C 450 -13, 450 67, 530 27"
-                    stroke="#cfebff"
-                    strokeWidth="3"
-                    fill="none"
-                />
-                <path
-                    ref={waveRefs[2]}
-                    d="M 70 27 C 150 7, 150 47, 300 27 C 450 7, 450 47, 530 27"
-                    stroke="#cfebff"
-                    strokeWidth="3"
-                    fill="none"
-                />
-                <path
-                    ref={waveRefs[3]}
-                    d="M 70 27 C 150 -8, 150 62, 300 27 C 450 -8, 450 62, 530 27"
-                    stroke="#cfebff"
-                    strokeWidth="3"
-                    fill="none"
-                />
-                <path
-                    ref={waveRefs[4]}
-                    d="M 70 27 C 150 2, 150 52, 300 27 C 450 2, 450 52, 530 27"
-                    stroke="#cfebff"
-                    strokeWidth="3"
-                    fill="none"
-                />
+        {/* Waves */}
+        <path
+          ref={waveRefs[0]}
+          d="M 70 27 C 150 -3, 150 57, 300 27 C 450 -3, 450 57, 530 27"
+          stroke="#cfebff"
+          strokeWidth="3"
+          fill="none"
+        />
+        <path
+          ref={waveRefs[1]}
+          d="M 70 27 C 150 -13, 150 67, 300 27 C 450 -13, 450 67, 530 27"
+          stroke="#cfebff"
+          strokeWidth="3"
+          fill="none"
+        />
+        <path
+          ref={waveRefs[2]}
+          d="M 70 27 C 150 7, 150 47, 300 27 C 450 7, 450 47, 530 27"
+          stroke="#cfebff"
+          strokeWidth="3"
+          fill="none"
+        />
+        <path
+          ref={waveRefs[3]}
+          d="M 70 27 C 150 -8, 150 62, 300 27 C 450 -8, 450 62, 530 27"
+          stroke="#cfebff"
+          strokeWidth="3"
+          fill="none"
+        />
+        <path
+          ref={waveRefs[4]}
+          d="M 70 27 C 150 2, 150 52, 300 27 C 450 2, 450 52, 530 27"
+          stroke="#cfebff"
+          strokeWidth="3"
+          fill="none"
+        />
 
-                {/* House on Fire icon at the end */}
-                <foreignObject x="520" y="20" width="60" height="60">
-                    <HouseOnFireIcon />
-                </foreignObject>
-            </svg>
-        </div>
-    );
+        {/* House on Fire icon at the end */}
+        <foreignObject x="520" y="20" width="60" height="60">
+          <HouseOnFireIcon />
+        </foreignObject>
+      </svg>
+    </div>
+  );
 }
--- development/helm-sucht-kopf/src/app/results/page.tsx
+++ development/helm-sucht-kopf/src/app/results/page.tsx
@@ -1,147 +1,165 @@
-'use client';
+"use client";
 
-import { useRouter } from 'next/navigation';
-import styles from '../styles/quiz.module.css';
-import styles2 from '../styles/jobs.module.css'
+import { useRouter } from "next/navigation";
+import styles from "../styles/quiz.module.css";
+import styles2 from "../styles/jobs.module.css";
 import JobCard from "@/app/components/jobs/JobCard";
-import React, {useState, useEffect} from "react";
-import { fetchJobs } from '@/app/api/jobs/fetchJobs';
-
+import React, { useState, useEffect } from "react";
+import { fetchJobs } from "@/app/api/jobs/fetchJobs";
 
 const Results = () => {
-    const [quizResult, setQuizResult] = useState<any>(null);
-    const [jobs, setJobs] = useState<any[]>([]);
-    const router = useRouter();
-    const [isLoading, setIsLoading] = useState(true);
+  const [quizResult, setQuizResult] = useState<any>(null);
+  const [jobs, setJobs] = useState<any[]>([]);
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(true);
 
-
-    useEffect(() => {
-        const storedResult = localStorage.getItem('quizResult');
-        if (storedResult) {
-            setQuizResult(JSON.parse(storedResult));
-        } else {
-            console.error('Quiz result not found in localStorage');
-            router.push('/quiz'); // Redirect back if result is missing
-        }
-    }, []);
-    useEffect(() => {
-        const loadJobs = async () => {
-            const jobsData = await fetchJobs();
-            console.log('Fetched Jobs:', jobsData);
-            setJobs(Object.values(jobsData));
-        };
-        loadJobs();
-    }, []);
-
-
-// Filter recommended jobs based on quiz result
-    const getRecommendedJobs = (type: 'direkt' | 'zukünftig') => {
-        if (!quizResult) {
-            console.warn('Quiz result is null or undefined');
-            return [];
-        }
-        const recommendedJobs = quizResult.result[type];
-        if (!recommendedJobs) {
-            console.warn(`No recommendations found for type: ${type}`);
-            return [];
-        }
-        console.log('Filtered Jobs:', recommendedJobs);
-        return jobs.filter((job) => recommendedJobs.includes(job.name));
+  useEffect(() => {
+    const storedResult = localStorage.getItem("quizResult");
+    if (storedResult) {
+      setQuizResult(JSON.parse(storedResult));
+    } else {
+      console.error("Quiz result not found in localStorage");
+      router.push("/quiz"); // Redirect back if result is missing
+    }
+  }, []);
+  useEffect(() => {
+    const loadJobs = async () => {
+      const jobsData = await fetchJobs();
+      console.log("Fetched Jobs:", jobsData);
+      setJobs(Object.values(jobsData));
     };
-    const futureJobs = getRecommendedJobs('zukünftig');
-    const currentJobs = getRecommendedJobs('direkt');
+    loadJobs();
+  }, []);
 
-    return (
-        <div className={styles.resultsContainer}>
-            {/* Display Immediate Opportunities */}
-            <h3 className={styles.centeredHeader}>Aktuelle Möglichkeiten:</h3>
-            {currentJobs.length > 0 ? (
-                <>
-                    <p className={styles.centeredText}>
-                        Diese Ausbildungen kannst Du direkt mit Deinem aktuellen Abschluss beginnen:
-                    </p>
-                    <div className={`${styles.jobList} md:grid-cols-2`}>
-                        {currentJobs.map(({id, name, slug, shortDesc}) => (
-                            <div key={slug}>
-                                <JobCard id={id} slug={slug} name={name} shortDesc={shortDesc}/>
-                            </div>
-                        ))}
-                    </div>
-                </>
-            ) : (
-                <p className={styles.centeredText}>
-                    Leider gibt es derzeit keine Ausbildungen, die direkt mit Ihrem aktuellen Abschluss begonnen werden
-                    können. Schauen Sie sich jedoch die zukünftigen Möglichkeiten an.
-                </p>
-            )}
+  // Filter recommended jobs based on quiz result
+  const getRecommendedJobs = (type: "direkt" | "zukünftig") => {
+    if (!quizResult) {
+      console.warn("Quiz result is null or undefined");
+      return [];
+    }
+    const recommendedJobs = quizResult.result[type];
+    if (!recommendedJobs) {
+      console.warn(`No recommendations found for type: ${type}`);
+      return [];
+    }
+    console.log("Filtered Jobs:", recommendedJobs);
+    return jobs.filter((job) => recommendedJobs.includes(job.name));
+  };
+  const futureJobs = getRecommendedJobs("zukünftig");
+  const currentJobs = getRecommendedJobs("direkt");
 
-            {/* Display Future Opportunities */}
-            {futureJobs.length > 0 && (
-                <>
-                    <h3 className={styles.centeredHeader2}>Zukünftige Möglichkeiten:</h3>
-                    <p className={styles.centeredText}>
-                        Diese Ausbildungen kannst Du nach weiteren Qualifikationen oder Abschlüssen beginnen:
-                    </p>
-                    <div className={`${styles.jobList} md:grid-cols-2`}>
-                        {futureJobs.map(({id, name, slug, shortDesc}) => (
-                            <div key={slug}>
-                                <JobCard id={id} slug={slug} name={name} shortDesc={shortDesc}/>
-                            </div>
-                        ))}
-                    </div>
-                </>
-            )}
+  return (
+    <div className={styles.resultsContainer}>
+      {/* Display Immediate Opportunities */}
+      <h3 className={styles.centeredHeader}>Aktuelle Möglichkeiten:</h3>
+      {currentJobs.length > 0 ? (
+        <>
+          <p className={styles.centeredText}>
+            Diese Ausbildungen kannst Du direkt mit Deinem aktuellen Abschluss
+            beginnen:
+          </p>
+          <div className={`${styles.jobList} md:grid-cols-2`}>
+            {currentJobs.map(({ id, name, slug, shortDesc }) => (
+              <div key={slug}>
+                <JobCard
+                  id={id}
+                  slug={slug}
+                  name={name}
+                  shortDesc={shortDesc}
+                />
+              </div>
+            ))}
+          </div>
+        </>
+      ) : (
+        <p className={styles.centeredText}>
+          Leider gibt es derzeit keine Ausbildungen, die direkt mit Ihrem
+          aktuellen Abschluss begonnen werden können. Schauen Sie sich jedoch
+          die zukünftigen Möglichkeiten an.
+        </p>
+      )}
 
-            {/* Restart Button */}
-            <div className={styles.buttonContainer}>
-                <button
-                    className={styles.restartButton}
-                    onClick={() => router.push('/quiz')} // Redirect back to the quiz intro page
-                >
-                    Quiz erneut starten
-                </button>
+      {/* Display Future Opportunities */}
+      {futureJobs.length > 0 && (
+        <>
+          <h3 className={styles.centeredHeader2}>Zukünftige Möglichkeiten:</h3>
+          <p className={styles.centeredText}>
+            Diese Ausbildungen kannst Du nach weiteren Qualifikationen oder
+            Abschlüssen beginnen:
+          </p>
+          <div className={`${styles.jobList} md:grid-cols-2`}>
+            {futureJobs.map(({ id, name, slug, shortDesc }) => (
+              <div key={slug}>
+                <JobCard
+                  id={id}
+                  slug={slug}
+                  name={name}
+                  shortDesc={shortDesc}
+                />
+              </div>
+            ))}
+          </div>
+        </>
+      )}
+
+      {/* Restart Button */}
+      <div className={styles.buttonContainer}>
+        <button
+          className={styles.restartButton}
+          onClick={() => router.push("/quiz")} // Redirect back to the quiz intro page
+        >
+          Quiz erneut starten
+        </button>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <div className={styles2.undecidedDiv}>
+          <div
+            style={{
+              width: "100%",
+              maxWidth: "700px",
+              marginTop: "0.5rem",
+            }}
+          >
+            <div
+              style={{
+                position: "relative",
+                width: "100%",
+              }}
+            >
+              <img
+                src={`./assets/video/Azubis.jpg`}
+                alt="Noch unsicher 1"
+                style={{
+                  borderRadius: "2rem",
+                  filter: "brightness(0.7)",
+                  maxWidth: "100vw",
+                  objectFit: "cover",
+                  objectPosition: "center 15%",
+                  height: "30vh",
+                }}
+              />
+              <div className={styles2.undecidedText}>
+                Entdecke deine Ausbildung – <br />
+                im interaktiven Video-Rundgang!
+              </div>
+              <button
+                onClick={() => (window.location.href = "/tour")}
+                className={styles2.startButton}
+              >
+                Starten!
+              </button>
             </div>
-            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width:'120%'}}>
-                <div className={styles2.undecidedDiv}>
-                    <div
-                         style={{
-                             width: '100%',
-                             maxWidth: '700px',
-                             marginTop: '0.5rem'
-                         }}>
-                        <div style={{
-                            position: 'relative',
-                            width: '100%',
-                        }}>
-                            <img
-                                src={`./assets/video/Azubis.jpg`}
-                                alt="Noch unsicher 1"
-                                style={{
-                                    borderRadius: '2rem',
-                                    filter: 'brightness(0.7)',
-                                    width: '85vw',
-                                    objectFit: 'cover',
-                                    objectPosition: 'center 15%',
-                                    height: '30vh',
-                                }}
-                            />
-                            <div className={styles2.undecidedText}>
-                                Entdecke deine Ausbildung – <br/>
-                                im interaktiven Video-Rundgang!
-                            </div>
-                            <button
-                                onClick={() => window.location.href = '/tour'}
-                                className={styles2.startButton}
-                            >
-                                Starten!
-                            </button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-</div>
-)
-    ;
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Results;
